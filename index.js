d82f0e8b48d50d87b1a1c90d6c8b2da080dc3d36
Demilade111 <demiladealuko111@gmail.com>
2024-06-30 23:48:32 +0100

    chore: Update routing in index.js for /api/hello endpoint

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(requestIp.mw());
-app.use("/", (req, res, next) => {
-  res.send("Welcome to HNG-Tasks!");
-  next();
-});
 app.use("/api/hello", helloRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to HNG-Tasks!");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
