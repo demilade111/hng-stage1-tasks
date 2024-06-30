602ba64464263a7dbd700dabd11f6369099fbe03
Demilade111 <demiladealuko111@gmail.com>
2024-06-30 23:44:11 +0100

    chore: Update port handling in index.js and package.json

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const requestIp = require('request-ip');
-const helloRoute = require('./routes/hello');
+require("dotenv").config();
+const express = require("express");
+const requestIp = require("request-ip");
+const helloRoute = require("./routes/hello");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(requestIp.mw());
-
-app.use('/api/hello', helloRoute);
+app.use("/", (req, res, next) => {
+  res.send("Welcome to HNG-Tasks!");
+  next();
+});
+app.use("/api/hello", helloRoute);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
